fd2b963289d852b259c51c3dec169f4fe99afbb2
Angelique15 <mar.angel.go15@gmail.com>
2023-08-09 16:00:06 -0500

    routes setup

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,70 @@
-const express = require('express')
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const contactsModel = require('../../models/contacts');
 
-const router = express.Router()
+const contactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// GET /api/contacts
+router.get('/', (req, res) => {
+  const contacts = contactsModel.listContacts();
+  res.status(200).json(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// GET /api/contacts/:id
+router.get('/:id', (req, res) => {
+  const contact = contactsModel.getContactById(req.params.id);
+  if (contact) {
+    res.status(200).json(contact);
+  } else {
+    res.status(404).json({ message: 'Not found' });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// POST /api/contacts
+router.post('/', (req, res) => {
+  const { error } = contactSchema.validate(req.body);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (error) {
+    res.status(400).json({ message: error.details[0].message });
+    return;
+  }
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { name, email, phone } = req.body;
+  const newContact = contactsModel.addContact({ name, email, phone });
+  res.status(201).json(newContact);
+});
 
-module.exports = router
+// DELETE /api/contacts/:id
+router.delete('/:id', (req, res) => {
+  const result = contactsModel.removeContact(req.params.id);
+  if (result) {
+    res.status(200).json({ message: 'Contact deleted' });
+  } else {
+    res.status(404).json({ message: 'Not found' });
+  }
+});
+
+// PUT /api/contacts/:id
+router.put('/:id', (req, res) => {
+  const { error } = contactSchema.validate(req.body);
+
+  if (error) {
+    res.status(400).json({ message: error.details[0].message });
+    return;
+  }
+
+  const updatedContact = contactsModel.updateContact(req.params.id, req.body);
+  if (updatedContact) {
+    res.status(200).json(updatedContact);
+  } else {
+    res.status(404).json({ message: 'Not found' });
+  }
+});
+
+module.exports = router;
+
